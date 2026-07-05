073d826a2926db7d1dc97268212b4b8d736c6adb
Himani Joshi <himanijoshi28@gmail.com>
2026-07-05 23:18:40 +0530

    UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
+import axios from "axios"
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+
+  //wont render since the data is not a state variable.
+  //You have to specisfically tell react  which are state variables.
+  // let data;
+  // axios.get("https://jsonplaceholder.typicode.com/todos")
+  //   .then(response => {
+  //     data = response.data;
+  //   })
+
+  // How to declare a State variable to tell react to rerender when changes happens/ state of that variable updates 
+
+  let [data, setData] = useState<any[]>([]); // initialised as empty array.
+
+
+  axios.get("https://jsonplaceholder.typicode.com/todos")
+    .then(response => {
+      setData(response.data);
+    })
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="">
 
-      <div className="ticks"></div>
+        {/* {
+          JSON.stringify(data)
+        } */}
+        <h1>Todos: </h1>
+        {data?.map((todo) =>
+          //  (
+          // <div style={{ backgroundColor: "pink", margin: "5px", padding: "5px", border: "2px solid black", borderRadius: "10px" }}>{todo?.title}</div>
+          // )
+          <Todo key={todo.id} title={todo.title}></Todo>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        )}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+
+      </div></>
   )
 }
+
+function Todo(props: any) {
+  return <div style={{ backgroundColor: "pink", margin: "5px", padding: "5px", border: "2px solid black", borderRadius: "10px" }}>{props.title}</div>
+}
+
+
+
+
+
+
+
 
 export default App
